ad1011dd7cacb7dd05fb5f3394aa0176db3def95
Tejus Parikh <tejus@vijedi.net>
2009-02-25 18:18:50 -0500

    adding the latest version of the appcelerator release, instead of the inhouse modified one

--- public/widgets/app_script-ptek/app_script.js
+++ public/widgets/app_script-ptek/app_script.js
@@ -1,20 +1,23 @@
-
 /*
- * Copyright 2006-2008 Appcelerator, Inc.
+ * This file is part of Appcelerator.
+ *
+ * Copyright (C) 2006-2008 by Appcelerator, Inc. All Rights Reserved.
+ * For more information, please visit http://www.appcelerator.org
+ *
+ * Appcelerator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
-
 
 
 Appcelerator.Widget.Script =
@@ -29,7 +32,7 @@ Appcelerator.Widget.Script =
 	},
 	getVersion: function()
 	{
-		return '__VERSION__';
+		return 1.0;
 	},
 	getSpecVersion: function()
 	{
@@ -64,12 +67,19 @@ Appcelerator.Widget.Script =
             description: "May be used to execute the script's content."
         }];
 	},
-	execute: function(id,parameterMap,data,scope,version,customdata,direction,type)
+	execute: function(id,parameterMap,data,scope,version)
 	{
-		var code = parameterMap['code'];
-		var script = code.toFunction(true);
-		if (script == true) return;
-		script.call({data:data||{},scope:scope,version:version,type:type,direction:direction});
+	    try
+	    {	        
+    		var code = parameterMap['code'];
+    		var script = code.toFunction(true);
+    		if (script == true) return;
+    		script.call({data:data||{},scope:scope,version:version});
+        } catch (exe)
+        {
+            console.error("Error: " + exe);
+            console.error("in app:script: " + parameterMap['code']);
+        }
 	},
 	compileWidget: function(params)
 	{
