f4abcbd09600df5d9b15ec8becf1f8667277b942
bahzuercher <az@zuerchtech.com>
2009-03-10 19:17:34 -0400

    got the test working with the lastest version of appcelerator

--- public/appcelerator-2.3/javascripts/application.js
+++ public/appcelerator-2.3/javascripts/application.js
@@ -1,7 +1,7 @@
 $MQL('l:app.compiled',function(type,msg,datatype,from) {
     APP_TIMERS.DOM_LOADED = new Date();
 });
-$MQL('l:mixed.loaded then execute',function(type,msg,datatype,from) {
+$MQL('l:mixed.loaded',function(type,msg,datatype,from) {
     APP_TIMERS.MIXED_RENDER = new Date();
     $MQ('l:load.big.file', {});
 });
@@ -21,8 +21,35 @@ $MQL('r:get.votes.response',function(type,msg,datatype,from) {
     $MQ('l:mixed.loaded');
   }, 5);
 });
+jQuery.extend({
+ getURLParam: function(strParamName){
+	  var strReturn = "";
+	  var strHref = window.location.href;
+	  var bFound=false;
+	  
+	  var cmpstring = strParamName + "=";
+	  var cmplen = cmpstring.length;
+
+	  if ( strHref.indexOf("?") > -1 ){
+	    var strQueryString = strHref.substr(strHref.indexOf("?")+1);
+	    var aQueryString = strQueryString.split("&");
+	    for ( var iParam = 0; iParam < aQueryString.length; iParam++ ){
+	      if (aQueryString[iParam].substr(0,cmplen)==cmpstring){
+	        var aParam = aQueryString[iParam].split("=");
+	        strReturn = aParam[1];
+	        bFound=true;
+	        break;
+	      }
+	      
+	    }
+	  }
+	  if (bFound==false) return null;
+	  return strReturn;
+	}
+});
+
 $MQL('l:votes.loaded',function(type,msg,datatype,from) {
-  var items = ''+ Appcelerator.Parameters.get('items');
+  var items = $.getURLParam("items");
   if (items=='')
     $MQ('r:get.votes.request');
   else
