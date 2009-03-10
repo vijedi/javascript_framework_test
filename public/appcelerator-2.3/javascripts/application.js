d233474ce5484a44dcdbb1fcf0d984bb050cd3fa
bahzuercher <az@zuerchtech.com>
2009-03-10 19:59:20 -0400

    added the crazy big file to the 2.3 release to run properly

--- public/appcelerator-2.3/javascripts/application.js
+++ public/appcelerator-2.3/javascripts/application.js
@@ -1,26 +1,3 @@
-$MQL('l:app.compiled',function(type,msg,datatype,from) {
-    APP_TIMERS.DOM_LOADED = new Date();
-});
-$MQL('l:mixed.loaded',function(type,msg,datatype,from) {
-    APP_TIMERS.MIXED_RENDER = new Date();
-    $MQ('l:load.big.file', {});
-});
-$MQL('l:big.file.loaded',function(type,msg,datatype,from) {
-    APP_TIMERS.BIG_FILE_LOADED = new Date();
-    $MQ('l:load.dynamic.big.file', {});
-});
-$MQL('l:dynamic.big.file.loaded',function(type,msg,datatype,from) {
-    APP_TIMERS.BIG_DYNAMIC_FILE_LOADED = new Date();
-    $MQ('l:load.crazy.big.file', {});
-    APP_TIMERS.finish_test();
-});
-
-//votes.html content file
-$MQL('r:get.votes.response',function(type,msg,datatype,from) {
-  setTimeout(function(){
-    $MQ('l:mixed.loaded');
-  }, 5);
-});
 jQuery.extend({
  getURLParam: function(strParamName){
 	  var strReturn = "";
@@ -46,6 +23,39 @@ jQuery.extend({
 	  if (bFound==false) return null;
 	  return strReturn;
 	}
+});
+$MQL('l:app.compiled',function(type,msg,datatype,from) {
+    APP_TIMERS.DOM_LOADED = new Date();
+});
+$MQL('l:mixed.loaded',function(type,msg,datatype,from) {
+    APP_TIMERS.MIXED_RENDER = new Date();
+    $MQ('l:load.big.file', {});
+});
+$MQL('l:big.file.loaded',function(type,msg,datatype,from) {
+    APP_TIMERS.BIG_FILE_LOADED = new Date();
+    $MQ('l:load.dynamic.big.file', {});
+});
+$MQL('l:dynamic.big.file.loaded',function(type,msg,datatype,from) {
+    APP_TIMERS.BIG_DYNAMIC_FILE_LOADED = new Date();
+    var test_crazylargefile = $.getURLParam("test_crazylargefile");
+    if (test_crazylargefile=='true') {
+      $MQ('l:load.crazy.big.file', {});
+    } else {
+      APP_TIMERS.CRAZY_LARGE_FILE_LOADED = APP_TIMERS.BIG_DYNAMIC_FILE_LOADED;
+      APP_TIMERS.finish_test();
+    }
+});
+$MQL('l:crazy.big.file.loaded',function(type,msg,datatype,from) {
+      APP_TIMERS.CRAZY_LARGE_FILE_LOADED = new Date();
+      APP_TIMERS.finish_test();
+});
+
+
+//votes.html content file
+$MQL('r:get.votes.response',function(type,msg,datatype,from) {
+  setTimeout(function(){
+    $MQ('l:mixed.loaded');
+  }, 5);
 });
 
 $MQL('l:votes.loaded',function(type,msg,datatype,from) {
